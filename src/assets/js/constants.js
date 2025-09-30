c2c401791e48302d420b62e8cea50cbee1e60e14
Eltayeb Ibrahim <80187951+abuomerSD@users.noreply.github.com>
2025-09-30 11:22:22 +0300

    Update constants.js

--- src/assets/js/constants.js
+++ src/assets/js/constants.js
@@ -1,8 +1,5 @@
 // server
 
-export const API_URL = 'http://109.106.244.174:5000/api/v1/';
-export const STATIC_FILES_URL = 'http://109.106.244.174:5000/';
-
 // local
 
 // export const API_URL = 'http://localhost:5000/api/v1/';
